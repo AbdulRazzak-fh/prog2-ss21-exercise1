5b16257f479ffdb415f59c56a90cffbd63e074e3
Abdul Razzak <abdul.razzak@stud.fh-campuswien.ac.at>
2022-03-21 20:23:21 +0100

    finished

--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -7,42 +7,58 @@ public class Result
     public static List<Integer> gradingStudents(List<Integer> grades)
     {
         List<Integer> result = new ArrayList<>();
-        if (grades.size() >= 1 && grades.size() <= 60)
-        {
-            for (Integer i : grades)
-            {
-                if (i < 0 || i > 100 )
-                {
-                    System.out.println("Number should be in range of 0 to 100");
-                }
-                else if(i < 38)
-                {
-                    System.out.println("Fail");
-                }
-                else
-                {
-                    if(i % 5 == 0 || i % 5 <= 3)
-                    {
-                        result.add(i);
-                        System.out.println(i);
-                    }
-                    else if (i % 5 > 3)
-                    {
-                        result.add((i + (5 - (i % 5) )));
-                        System.out.println((i + (5 - (i % 5) )));
+        try {
+
+            if (grades.size() >= 1 && grades.size() <= 60) {
+                for (Integer i : grades) {
+                    if (i < 0 || i > 100) {
+                        System.out.println("Number should be in range of 0 to 100");
+                    } else if (i < 38) {
+                        System.out.println("Fail");
+                    } else {
+                        if (i % 5 == 0 || (i % 5) <= 3) {
+                            result.add(i);
+                            System.out.println(i);
+                        } else {
+                            result.add((i + (5 - (i % 5))));
+                            System.out.println((i + (5 - (i % 5))));
+                        }
                     }
                 }
+            } else {
+                System.out.println("Problem with number of Grades!");
             }
+            return result;
         }
-        else
+        catch (NullPointerException e)
         {
-            System.out.println("Problem with number of Grades!");
+            System.out.println("Null pointer Exception");
+            return result;
         }
-        return result;
+
+    }
+    public static boolean checkinRange(int i)
+    {
+       return (i < 0 || i > 100);
+    }
+    public static int checkResult(int i)
+    {
+        if ((i % 5) <= 3) {
+            return i;
+        }
+        return ((i + (5 - (i % 5))));
+    }
+    public static boolean checkNull (List<Integer> list)
+    {
+        return list.size() != 0 && list.size() <= 60;
+    }
+    public static boolean checkLess38(int i)
+    {
+        return (i < 38);
     }
 
     public static void main(String[] args)
     {
-        System.out.println(gradingStudents(Arrays.asList()));
+
     }
 }
--- src/test/java/ResultTest.java
+++ src/test/java/ResultTest.java
@@ -51,21 +51,77 @@ public class ResultTest
     void testgradingStudents_numberofGradesmorethan60()
     {
         List<Integer> List = Arrays.asList(101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90);
-        Result.gradingStudents(List);
-        List<Integer> actual = new ArrayList<>();
-        List<Integer> expected = new ArrayList<>();
+        List<Integer> actual = Result.gradingStudents(List);
+        List<Integer> expected = Arrays.asList();
         assertEquals(expected, actual);
 
     }
-    @Test
-    @DisplayName("5. Number of grades 0")
-    void testgradingStudents_numberofGrades0()
+
+    /*@Test
+    @DisplayName("5. Check NullPointer")
+    void testgradingStudents_NullPointer()
     {
-        List<Integer> List = new ArrayList<>();
-        Result.gradingStudents(List);
-        //List<Integer> actual = new ArrayList<>();
-        List<Integer> expected = new ArrayList<>();
-        assertNotNull(expected);
+        List<Integer> List = Arrays.asList();
+
+        String actual = "Null pointer Exception";
+        String expected = "Null pointer Exception";
+        assertEquals(expected, actual);
+    } */
+    @Test
+    @DisplayName("6. Check in Range")
+    void testgradingStudents_inRange()
+    {
+       assertTrue(Result.checkinRange(106));
+    }
+    @Test
+    @DisplayName("7. Check minus")
+    void testgradingStudents_minus()
+    {
+        assertTrue(Result.checkinRange(-1));
+    }
+    @Test
+    @DisplayName("8. Check high")
+    void testgradingStudents_too_high()
+    {
+        assertTrue(Result.checkinRange(101));
+    }
+    @Test
+    @DisplayName("9. Check result")
+    void testgradingStudents_result()
+    {
+        assertEquals(55,Result.checkResult(54));
+    }
+    @Test
+    @DisplayName("10. Check NullPointerFalse")
+    void testgradingStudents_NullPointerFalse()
+    {
+        List<Integer> List = Arrays.asList(54);
+        assertTrue(Result.checkNull(List));
+    }
+    @Test
+    @DisplayName("11. Check NullPointerFalse")
+    void testgradingStudents_NullPointerTrue()
+    {
+        assertFalse(Result.checkNull(new ArrayList<>()));
+    }
+    @Test
+    @DisplayName("11. Check More than 60")
+    void testgradingStudents_morethan60()
+    {
+        List<Integer> List = Arrays.asList(101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90,101,65,74,61,90);
+        assertFalse(Result.checkNull(List));
+    }
+    @Test
+    @DisplayName("13. Check less than 38")
+    void testgradingStudents_less38true()
+    {
+        assertTrue(Result.checkLess38(37));
+    }
+    @Test
+    @DisplayName("14. Check less than 38")
+    void testgradingStudents_less38False()
+    {
+        assertFalse(Result.checkLess38(39));
     }
 
 }
